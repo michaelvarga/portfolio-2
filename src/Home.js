f4b2f1a6700f64ade7fbb7e494b1c7aef6be8d3a
Mike Varga <mikevarga07@gmail.com>
2022-12-06 14:34:08 -0600

    bootstrap style updates

--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,9 @@ const Home = () => {
   return (
     <div className="fluid-container vh-100 App-header d-flex position-relative">
       <div className="circle"/>
-      <div>
+      <div className="container">
           <h6>HELLO, I'M</h6>
-          <h1>Mike</h1>
+          <h1 className="text-primary">Mike</h1>
           <p>
             <a className="btn btn-primary btn-lg" href="#" role="button">
               Learn more
