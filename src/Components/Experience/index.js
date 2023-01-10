09cf6215fb0ecf911dc016735ae826352d5bcaa8
Mike Varga <mikevarga07@gmail.com>
2023-01-10 16:26:12 -0600

    Updated the projects section, still working on styling it

--- src/Components/Experience/index.js
+++ src/Components/Experience/index.js
@@ -4,49 +4,22 @@ import "./index.scss";
 const Experience = () => {
   return (
     <div className="fluid-container min-vh-100 experience p-5">
-      <div>
-        <div>
-          <h3>Experience</h3>
-          <div>
-            <h4>Accenture</h4>
-            <div>
-              <h5>Lead Technical Consultant</h5>
-            </div>
-            <div>
-              <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
-            </div>
-          </div>
-          <div>
-            <h4>Accenture</h4>
-            <div>
-              <h5>Software Developer</h5>
-            </div>
-            <div>
-              <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
-            </div>
-          </div>
+      <div className="row">
+        <div className="col-md-4">
+          <h3>Over the past six years,</h3>
+          <p>
+            I've build products for companies and governments around the US,
+            ranging from reporting tools to complex features for enterprise apps
+            with a focus on exceptional user experiences.
+            <br/><br/>
+            I got my bachelors degree in Statistics from the <span>University of Illinois at Urbana-Champaign</span> in 2017, and joined the <span>Cook County State's Attorney's Office</span> in Chicago as their first ever data analyst. Faced with overwhelming amounts of violence in the city of Chicago and calls for reform, I was brought in to find creative ways to leverage data and technology to drive prosecutorial change. I used Tableau, ArcGIS, R, and SAS to create reporting tools and social network maps of violent offenders, which were used to prosecute several high-priority offenders.
+            <br/><br/>
+            Then I worked as a Technology Consultant for <span>Accenture</span>, where I was a developer on a project with the Department of Defense. My team built an app that modernized the way the federal government issues security clearances to personnel across the entire federal government, eliminating bottlenecks in the existing process and enabling millions of employees and contractors to get their security clearance significantly quicker.
+            <br/><br/>
+            After discovering my passion for software development at Accenture, I left the company to join a full time, immersive web development bootcamp at Fullstack Academy with the intention of transitioning careers from consulting to software engineering.
+          </p>
         </div>
-        <div>
-          <h3>Education</h3>
-          <div>
-            <h4>University of Illinois at Urbana-Champaign</h4>
-            <p>Bachelor of Arts in Statistics, Economics</p>
-            <p>August 2013 - May 2017</p>
-          </div>
-          <div>
-            <h4>Fullstack Academy</h4>
-            <p>Certificate in Fullstack Web Development</p>
-            <p>March 2022 - July 2022</p>
-          </div>
-        </div>
+        <div className="col-md-8"></div>
       </div>
     </div>
   );
--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,10 +1,80 @@
 import React from 'react'
 import './index.scss'
+import { GitHub, Launch, YouTube } from "@mui/icons-material"
+
+const liStyle = "list-inline-item me-3"
 
 const Projects = () => {
   return (
-    <div>
+    <div className="fluid-container min-vh-100 p-5 projects">
       <h2>Some Things I've Built</h2>
+      <div>
+        <div>
+          <h3>Happilist</h3>
+          <p>A full-stack social media app that encourages you to be productive and democratize your todo lists amongst your friends. This mobile app has some similar features of traditional social media apps, including authentication, sending and approving friend requests, uploading stories, viewing and interacting with a user feed, and more. It uses a Tensorflow.js for content moderation and Google Cloud storage for media.</p>
+          <ul className="list-inline mt-2">
+            <li className={liStyle}>React Native</li>
+            <li className={liStyle}>Expo</li>
+            <li className={liStyle}>React Hooks</li>
+            <li className={liStyle}>NoSQL</li>
+            <li className={liStyle}>Google Firebase</li>
+            <li className={liStyle}>Tensorflow.js</li>
+            <li className={liStyle}>Styled Components</li>
+          </ul>
+
+          <a href="https://github.com/2204-cookiemonsters-capstone/amazing-app" target="_blank" rel="noreferrer" aria-label="GitHub"><GitHub/></a>
+          <a href="https://www.youtube.com/watch?v=qvU4MeqDAZk&ab_channel=MikeVarga" target="_blank" rel="noreferrer"><YouTube/></a>
+        </div>
+        <div>
+          <h3>Grace's Greens</h3>
+          <p>A full-stack e-commerce site that sells plants. Features account authentication and guest/user cart and checkout.</p>
+          <ul className="list-inline mt-2">
+            <li className={liStyle}>React</li>
+            <li className={liStyle}>Redux</li>
+            <li className={liStyle}>Express</li>
+            <li className={liStyle}>PostgreSQL</li>
+            <li className={liStyle}>Sequelize</li>
+            <li className={liStyle}>Bcrypt</li>
+            <li className={liStyle}>JSON Web Tokens</li>
+          </ul>
+          <a href="https://github.com/FullStack-Sisko/GraceShopper" target="_blank" rel="noreferrer" aria-label="GitHub"><GitHub/></a>
+        </div>
+        <div>
+          <h3>National Background Investigation Services (NBIS)</h3>
+          <p>An enterprise application for the DoD that modernizes the way they issue over one million security clearances annually.</p>
+          <ul className="list-inline mt-2">
+            <li className={liStyle}>Pegasystems</li>
+            <li className={liStyle}>Javascript</li>
+            <li className={liStyle}>HTML</li>
+            <li className={liStyle}>CSS</li>
+          </ul>
+          <a href="https://www.dcsa.mil/is/nbis/" target="_blank" rel="noreferrer" aria-label="NBIS"><Launch/></a>
+        </div>
+        <div>
+          <h3>Handpose</h3>
+          <p>Uses a Tensorflow maching learning library to detect points on a users hand through their webcam, and detects hand gestures that correspond to popular emojis. This is being refactored to detect the American Sign Language alphabet to spell out words using just a user's webcam.
+          </p>
+          <ul className="list-inline mt-2">
+            <li className={liStyle}>React</li>
+            <li className={liStyle}>React Hooks</li>
+            <li className={liStyle}>Tensorflow.js</li>
+          </ul>
+          <a href="https://github.com/michaelvarga/Handpose" target="_blank" rel="noreferrer" aria-label="GitHub"><GitHub/></a>
+        </div>
+        <div>
+          <h3>Portfolio Site</h3>
+          <div className="tools-used">
+            <ul className="list-inline mt-2">
+              <li className={liStyle}>React</li>
+              <li className={liStyle}>Bootstrap</li>
+              <li className={liStyle}>Sass</li>
+              <li className={liStyle}>Material UI</li>
+              <li className={liStyle}>Firebase</li>
+            </ul>
+            <a href="https://github.com/michaelvarga/portfolio-2" target="_blank" rel="noreferrer" aria-label="GitHub"><GitHub/></a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
